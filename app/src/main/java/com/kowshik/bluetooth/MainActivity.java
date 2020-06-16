dd050fa8dd8617ad27a6e42cd4b93770bc70eda5
blackcloud007 <kowshikkarmagam@gmail.com>
2020-06-16 13:19:55 +0530

    nav feature ..commit1
    drawer added
    title needs to be hidden

--- app/src/main/java/com/kowshik/bluetooth/MainActivity.java
+++ app/src/main/java/com/kowshik/bluetooth/MainActivity.java
@@ -5,32 +5,35 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
 
     public static final String bsocket = "com.kowshik.bluetooth.socket";
 
     Button listen, listDevices;
     ListView listView;
     TextView status;
-    //EditText writeMsg;
+
 
     BluetoothAdapter bluetoothAdapter;
     BluetoothDevice[] btArray;
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewByIdes();
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
